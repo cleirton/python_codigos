ada14782775dbc0b336b4e20ce87e5ec409a0648
Cleirton Gomes de Brito <cleirton.gomes@gmail.com>
2022-10-27 15:26:49 -0300

    Create hello_world.py
    algumas formas de usar a função print e os comentários

---
+++ 01.hello_world/hello_world.py
@@ -0,0 +1,10 @@
+#  hello_world
+#  comentários e função print
+
+#  esta função imprime a string no console
+print("hello world")
+print('podemos usar aspas simples e aspas duplas')
+print('"vendo aspas duplas dentro da string"')
+print("'vendo aspas simples dentro da string'")
+print("para pular a linha \n")
+print("para mostrar a barra invertida \\")
